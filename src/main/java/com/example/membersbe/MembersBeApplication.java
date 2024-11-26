a9302401c99c03318c54a643794636c01a4497e9
Morten Ingman <morten.ingman@yahoo.com>
2024-11-26 09:59:36 +0100

    First

---
+++ src/main/java/com/example/membersbe/MembersBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.membersbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembersBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MembersBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/membersbe/MembersBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.membersbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembersBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
